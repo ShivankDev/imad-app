635191a1bf16c25185bfce6f87ff54ae4056663f
ShivankDev <30679936+ShivankDev@users.noreply.github.com>
2017-08-06 08:57:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/articeOne'),function (req, res) {
+    res.send("You are served with article one");
+};
+app.get('/articeTwo'),function (req, res) {
+    res.send("You are served with article two");
+};
+app.get('/articeThree'),function (req, res) {
+    res.send("You are served with article three");
+};
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
